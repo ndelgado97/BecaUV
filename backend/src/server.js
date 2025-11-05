55f8fd6541864cc634f7a30a9a55fbddfc0c874e
Slimpler <70767605+Slimpler@users.noreply.github.com>
2025-11-05 10:09:16 -0300

    subiendo

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 // Middlewares
 app.use(cors({
     /* origin: 'http://frontend_becasuv:80', */
-    origin: 'https://becauv-production.up.railway.app',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
